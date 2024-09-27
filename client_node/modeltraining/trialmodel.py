75b3debc47efb0204e7a03e0875b3adc512d7f82
Sahilgill24 <gursahilgill24@gmail.com>
2024-09-27 10:00:26 +0530

    communication b/w aptos and backend

---
+++ client_node/modeltraining/trialmodel.py
@@ -0,0 +1,5 @@
+# this would be a trial model to be tested
+# and see if it works and do the pderson commitments on it and then send it to the blockchain along with
+# the commitment generators to make it private and secure
+# as the blockchain is the one that is aggregating the final results
+# so the challenge is to provide the blockchain with the commitment generators
