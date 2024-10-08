87437c6b6e33d58dbf146027c008285bba867ce8
Sahilgill24 <gursahilgill24@gmail.com>
2024-10-09 01:40:05 +0530

    changes

---
+++ client_node/modeltraining/normalizer.py
@@ -0,0 +1,2 @@
+# this is used for normalizing and calculating the decimals etc. while calcuating 
+# the decimals and then normalizing the loss etc. for the pederson commitments
--- filehandler/uploader.js
+++
@@ -1,2 +0,0 @@
-// file storage to IPFS
- 
