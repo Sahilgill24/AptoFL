bb5e9b87984eedc9fa6b834bd00be0374966a6eb
Sahilgill24 <gursahilgill24@gmail.com>
2024-10-08 22:47:31 +0530

    dataset upload

--- client_node/server.py
+++ client_node/server.py
@@ -1,13 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template,redirect
 
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# this would be the endpoint to send data for prediction
 @app.route('/model_trainer', methods=['POST'])
 def model_trainer():
-    pass
+    if (request.method == 'POST'):
+        f= request.files['file']
+        f.save(f'trialdata/{f.filename}')
+        return redirect('/')
+        
 
-  
+
+@app.route('/predict', methods=['POST'])
+def training():
+    return render_template('predict.html')
+
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
